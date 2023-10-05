c5c5b55a0023b294fe14f31d1adccf287f6419b3
Jesus Belizario <belizarioja@gmail.com>
2023-10-05 19:06:02 -0400

    Mostrar comprobante

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,4 +1,5 @@
 module.exports = {
-  endpoint_path_v2: 'http://localhost:5001/',
+  // endpoint_path_v2: 'http://localhost:5001/',
+  endpoint_path_v2: 'http://192.168.0.100:5001/',
   version: '1.0.2'
 }
